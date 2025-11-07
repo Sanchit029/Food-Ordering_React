acc7492d39ee2bd0a2d1c217aa49292ddb67b20a
Sanchit Bishnoi <120033258+Sanchit029@users.noreply.github.com>
2025-11-07 15:59:20 +0530

    Update Meals.jsx

--- src/Components/Meals.jsx
+++ src/Components/Meals.jsx
@@ -9,7 +9,7 @@ export default function Meals() {
     data: loadedMeals,
     isLoading,
     error,
-  } = useHttp("http://localhost:3000/meals" , requestConfig,[]);
+  } = useHttp("https://food-ordering-react-1.onrender.com/meals" , requestConfig,[]);
 
   if (isLoading) {
     return <p className="center">Fetching Meals ...</p>;
